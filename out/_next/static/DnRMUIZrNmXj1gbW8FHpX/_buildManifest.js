d488509e68fc726ccd321874a730a94feb64af65
Reeul <chotnt741@gmail.com>
2022-03-09 07:26:45 +0900

    Deploy!

---
+++ out/_next/static/DnRMUIZrNmXj1gbW8FHpX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/336-bf84fe5efeb23863.js","static/chunks/pages/index-69188db61f0f52de.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- out/_next/static/INkwvhDNyT_EKgmHuPArg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/968-073dc3c8829dbbfc.js","static/chunks/pages/index-d8d3cac674c79892.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
