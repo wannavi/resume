2174610ec0ec970d5b471a08c3ed63a28550b1ee
Reeul <chotnt741@gmail.com>
2022-03-09 17:27:59 +0900

    Deploy!

--- _next/static/0tw7-8jnmVXL4PSzexKoe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/336-f593e9a08a5eb7d1.js","static/chunks/pages/index-b4b78da965c185da.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
